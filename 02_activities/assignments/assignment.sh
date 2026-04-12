1ec4afccb0d447af3c22dd895f0310bd62d9f573
anchitaa <anchitaaghag@gmail.com>
2026-04-11 20:10:49 -0400

    Revert Q7 answer from find & -delete back to rm

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -58,11 +58,8 @@ cp ./data/raw/*event*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
-#rm ./data/raw/*ipaddr* 
-#rm ./data/processed/user_logs/*ipaddr*
-
-find ./data/raw/ -type f -name "*ipaddr*.txt\|*ipaddr*.log" -delete
-find ./data/processed/user_logs/ -type f -name "*ipaddr*.txt\|*ipaddr*.log" -delete
+rm ./data/raw/*ipaddr* 
+rm ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
